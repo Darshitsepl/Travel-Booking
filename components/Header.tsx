2231dc879cc91239ab4716c3d097743c433c233e
Darshitsepl <sepl.darshittarapara@gmail.com>
2025-08-04 15:19:28 +0530

    Sign out function

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,27 @@
 'use client'
-import { signOut } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import React from 'react'
 
 const Header = () => {
+  const {data} = useSession();
+
   return (
     <div>
         <button type="button" onClick={async () => {
-          await signOut({
+          const userId = data?.user ? data?.user.userId as string : "";
+          const response =await (await fetch('/api/signout', {
+            method: "POST",
+            body: JSON.stringify({
+              userId
+            })
+          })).json();
+          if(response.status) {
+    await signOut({
             redirect: true,
             callbackUrl: "/login"
           })
+          }
+      
         }}>Log out</button>
     </div>
   )
