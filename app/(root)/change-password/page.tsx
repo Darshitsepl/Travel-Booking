fa35a864aeebf386c8b0059b160689d1a34c378e
Darshitsepl <sepl.darshittarapara@gmail.com>
2025-09-22 00:22:31 +0530

    Fix build issue

--- app/(root)/change-password/page.tsx
+++ app/(root)/change-password/page.tsx
@@ -1,13 +1,9 @@
 'use client'
-import { useAuth } from '@/context/Auth'
 import React from 'react'
 
 const ChangePassword = () => {
-  const {user} = useAuth();
 
-  /**
-   * If user type has google then provide the set password option
-   */
+ 
   return (
     <div>MyBooking</div>
   )
