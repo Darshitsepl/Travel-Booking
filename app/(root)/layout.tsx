33d02bacc3ac3e6526895c3bdf38bbaa69d50878
Darshitsepl <sepl.darshittarapara@gmail.com>
2025-09-21 23:03:52 +0530

    Improve User performance

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,41 +1,37 @@
 "use client";
 import Header from "@/components/Header";
-import Loading from "@/components/Loading";
 import PasswordAlertPopUp from "@/components/PasswordAlertPopUp";
 import { useAuth } from "@/context/Auth";
-import { GetUserProfile } from "@/lib/graphql/Query";
-import { GetUserProfileResponse } from "@/lib/graphql/type";
-import { useQuery } from "@apollo/client/react";
 import React, { useEffect } from "react";
 
 const MainLayoutProvider = ({ children }: { children: React.ReactNode }) => {
-	const { isOpen, setUser } = useAuth();
-	const { data, loading } = useQuery<GetUserProfileResponse>(GetUserProfile);
+	const { isOpen,setIsOpen } = useAuth();
 
-	useEffect(() => {
-		if (data && setUser) {
-			setUser(data?.GetUserProfile);
-		}
-	}, [data]);
+   useEffect(() => {
+  if (window.innerWidth < 786) {
+      console.log('running');
+      setIsOpen!(false);
+    }
+}, []);
 
-	if (loading) {
-		return <Loading />;
-	}
+
 
 	return (
 		<PasswordAlertPopUp>
 			<main
-				className={`grid ${
-					isOpen ? "grid-cols-[20%_80%]" : "grid-cols-[1fr]"
+				className={`grid 
+					 transition-all duration-300 ease-in-out
+					${
+					isOpen ? "grid-cols-[20%_75%]" : "grid-cols-[1fr]"
 				} gap-5 overflow-x-hidden`}
 			>
 				<div>
 					<Header />
 				</div>
 
-				<div className="m-2 md:m-12 px-2 py-6 md:px-4 overflow-x-hidden  shadow-[0_3px_8px_rgba(0,0,0,0.24)]">
+				<section  className="m-3 md:m-6 p-4 md:p-6 max-w-full mx-auto bg-white rounded-2xl shadow-[rgba(100,100,111,0.2)_0px_7px_29px_0px] overflow-y-auto">
 					{children}
-				</div>
+				</section>
 			</main>
 		</PasswordAlertPopUp>
 	);
